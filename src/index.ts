524a8f94e98104941044d318163baf6c269c62c1
Alexvtelles <outdsystem@gmail.com>
2022-12-21 19:08:20 -0300

    feat: initial server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("Olá Mundo!");
+import express from "express";
+import { config } from "dotenv";
+
+config();
+
+const app = express();
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => console.log(`listening on ${port}`));
+
+app.get("/", (req, res) => {
+  res.send("Hello world!");
+});
